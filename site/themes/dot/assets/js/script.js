a4540943183f26a5801c3a275885736a4fb5ac77
ereichl <ereichl@pascom.net>
2020-02-18 11:53:53 +0100

    PM-1538 - added clipboard + tooltip to documentation

--- site/themes/dot/assets/js/script.js
+++ site/themes/dot/assets/js/script.js
@@ -120,4 +120,25 @@
         tabPane.addClass('active');
     });
 
+    // Add anchor link button for every headline
+    var text, clip = new Clipboard('.anchor');
+    $("h1~h2,h1~h3,h1~h4,h1~h5,h1~h6").append(function(index, html){
+        var element = $(this);
+        var url = document.location.origin + document.location.pathname;
+        var link = url + "#"+element[0].id;
+        return " <span class='anchor' data-clipboard-text='"+link+"'>" +
+        "<i class='fas fa-link fa-lg'></i>" +
+        "</span>"
+        ;
+    });
+
+    $(".anchor").on('mouseleave', function(e) {
+        $(this).attr('aria-label', null).removeClass('tooltipped tooltipped-s tooltipped-w');
+    });
+
+    clip.on('success', function(e) {
+        e.clearSelection();
+        $(e.trigger).attr('aria-label', 'Link copied to clipboard!').addClass('tooltipped tooltipped-s');
+    });
+
 })(jQuery);
